420d52b4f47a46fa97e74069cef724ec93a539c2
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-01-28 10:41:37 -0500

    Initial Commit

---
+++ Programmatic-UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmatic-UI
+//
+//  Created by Christian Hurtado on 1/28/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
